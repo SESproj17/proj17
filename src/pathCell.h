821613af275fd22b7a334142aa254f83f7049e7e
Shalev <SESproj17>
2017-04-19 09:15:44 -0700

    Added the package

---
+++ src/pathCell.h
@@ -0,0 +1,28 @@
+#ifndef PATH_CELL
+#define PATH_CELL
+
+#include <ros/ros.h>
+#include <iostream>
+#include "cell.h"
+
+
+class pathCell: public cell {
+private:
+	float cost;
+	pathCell* lastCell;
+	vector<pathCell*> neighbors;
+
+public:
+	pathCell(myTuple* givenLocation,float givenProb,int givenId,float givenCost);
+	void setCost(float newCost);
+
+	void addNeighbors(pathCell* neigh);
+	
+	pathCell* getLastCell();
+	float getCost();
+	void setLastPathCell(pathCell* lastOne);
+	vector<pathCell*> getNeighbors();
+};
+
+
+#endif
